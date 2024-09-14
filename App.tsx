cd0cc5c2b076092a9b8fa6743f65b4598158b964
namdh03 <duonghoangnam503@gmail.com>
2024-09-14 16:19:29 +0700

    chore: add nativewind

--- App.tsx
+++ App.tsx
@@ -1,25 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import { storage } from '~utils/mmkv-storage';
+
+import './global.css';
 
 export default function App() {
   storage.set('test', 'ABC_XYZ');
   console.log(storage.getString('test'));
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex-1 justify-center items-center bg-gray-500'>
+      <Text className='text-center text-4xl color-yellow-500'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
