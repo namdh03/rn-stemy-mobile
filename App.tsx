3a3f20e64921bd01eff57bef11c424fdba8afe0a
namdh03 <duonghoangnam503@gmail.com>
2024-09-23 01:24:46 +0700

    chore: add config react native google signin

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import configs from '~configs';
 import RootNavigation from '~navigation/RootNavigation';
 
 import './global.css';
@@ -7,6 +9,12 @@ import './global.css';
 // Config react query
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 2 } },
+});
+
+// Config react native google sign in
+GoogleSignin.configure({
+  webClientId: configs.env.EXPO_PUBLIC_WEB_CLIENT_ID, // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
 });
 
 export default function App() {
