efc7e27e7c0bd1708b4ceb4c14f379450ad5ec40
lachimo <lachimolalasii1410@gmail.com>
2024-09-25 17:57:07 +0700

    feat: carousel banner

---
+++ src/components/customs/Carousel/Carousel.tsx
@@ -0,0 +1,25 @@
+import { FlatList, Image, View } from 'react-native';
+
+import images from '~assets/images';
+
+const Carousel = () => {
+  const data = [
+    { id: '1', imageUrl: images.carousel1 },
+    { id: '2', imageUrl: images.carousel1 },
+  ];
+  return (
+    <View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Image source={item.imageUrl} className='w-[315px] h-[158px] rounded-[10px]' />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={{ width: 15 }} />}
+        className='inline-flex pb-[27px]'
+      />
+    </View>
+  );
+};
+
+export default Carousel;
