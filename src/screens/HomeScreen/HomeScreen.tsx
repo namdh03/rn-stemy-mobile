b410475ecb1213c6676494087f8ae5323acd89eb
namdh03 <duonghoangnam503@gmail.com>
2024-09-25 19:27:23 +0700

    feat: product detail

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -12,7 +12,7 @@ import { removeAccessToken } from '~utils/token-storage';
 const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
   const unAuthenticate = useStore(useShallow((state) => state.unAuthenticate));
 
-  const goToProductDetail = (productId: string) => {
+  const goToProductDetail = (productId: number) => {
     navigation.navigate('ProductDetailStack', {
       screen: 'ProductDetailScreen',
       params: { id: productId },
@@ -30,7 +30,7 @@ const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
       <Button onPress={logout}>
         <Text>Logout</Text>
       </Button>
-      <Button className='mt-[4px]' onPress={() => goToProductDetail('1')}>
+      <Button className='mt-[4px]' onPress={() => goToProductDetail(1)}>
         <Text>Product Detail</Text>
       </Button>
     </View>
--- src/screens/ProductFeedbackScreen/ProductFeedbackScreen.tsx
+++ src/screens/ProductFeedbackScreen/ProductFeedbackScreen.tsx
@@ -5,9 +5,11 @@ import { Separator } from '~components/ui/separator';
 import { Text } from '~components/ui/text';
 import FeedbackItem from '~screens/ProductDetailScreen/components/FeedbackItem';
 import StarRating from '~screens/ProductDetailScreen/components/StarRating';
+import { ProductFeedbackScreenNavigationProps } from '~types/navigation';
+import calculateStarRatings from '~utils/calculateStarRatings';
 import nFormatter from '~utils/nFormatter';
 
-const ProductFeedbackScreen = () => {
+const ProductFeedbackScreen = ({ route }: ProductFeedbackScreenNavigationProps) => {
   return (
     <ScrollView
       contentContainerClassName='px-[25px] py-[30px] mx-auto w-full max-w-xl'
@@ -17,91 +19,51 @@ const ProductFeedbackScreen = () => {
       <View className='flex-row'>
         <View className='gap-[5px]'>
           <Text className='font-inter-bold text-foreground text-[30px] tracking-[3px]'>
-            4.6
+            {route.params.rating}
             <Text className='font-inter-regular text-foreground text-[14px] tracking-[3px]'>/5</Text>
           </Text>
-          <Text className='font-inter-regular text-foreground text-[14px] tracking-[0.2px]'>86 Reviews</Text>
+          <Text className='font-inter-regular text-foreground text-[14px] tracking-[0.2px]'>
+            {route.params.feedbacks.length || 0} Reviews
+          </Text>
         </View>
 
         <Separator orientation='vertical' className='ml-[18px] mr-[10px]' />
 
         <View className='flex-1 gap-[9px]'>
-          <View className='flex-row'>
-            <View className='flex-1 flex-row items-center gap-[10px]'>
-              <StarRating rating={5} nonFillColor='text-muted' />
-              <Progress max={100} value={90} className='flex-1 h-[4px]' indicatorClassName='bg-[#FFC120]' />
-              <Text className='font-inter-medium  text-right text-foreground text-[12px] tracking-[0.2px]'>
-                {nFormatter(12)}
-              </Text>
-            </View>
-          </View>
-          <View className='flex-row'>
-            <View className='flex-1 flex-row items-center gap-[10px]'>
-              <StarRating rating={4} nonFillColor='text-muted' />
-              <Progress max={100} value={90} className='flex-1 h-[4px]' indicatorClassName='bg-[#FFC120]' />
-              <Text className='font-inter-medium  text-right text-foreground text-[12px] tracking-[0.2px]'>
-                {nFormatter(12)}
-              </Text>
-            </View>
-          </View>
+          {calculateStarRatings(route.params.feedbacks)
+            .reverse()
+            .map((feedback) => (
+              <View key={feedback.star} className='flex-row'>
+                <View className='flex-1 flex-row items-center gap-[10px]'>
+                  <StarRating rating={feedback.star} nonFillColor='text-muted' />
+                  <Progress
+                    max={100}
+                    value={feedback.percentage}
+                    className='flex-1 h-[4px]'
+                    indicatorClassName='bg-[#FFC120]'
+                  />
+                  <Text className='font-inter-medium text-right text-foreground text-[12px] tracking-[0.2px]'>
+                    {nFormatter(feedback.count)}
+                  </Text>
+                </View>
+              </View>
+            ))}
         </View>
       </View>
 
       <View className='flex-1 gap-[20px] mt-[30px]'>
-        <FeedbackItem
-          id='1'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
-        <FeedbackItem
-          id='2'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
-        <FeedbackItem
-          id='3'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
-        <FeedbackItem
-          id='4'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
-        <FeedbackItem
-          id='5'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
-        <FeedbackItem
-          id='6'
-          avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
-          name='Dương Hoàng Nam'
-          time='2024-09-16T12:25:46.000Z'
-          rating={4}
-          comment='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          nonFillColor='text-muted'
-        />
+        {route.params.feedbacks.map((feedback) => (
+          <FeedbackItem
+            key={feedback.id}
+            id={feedback.id}
+            avatar='https://ispacedanang.edu.vn/wp-content/uploads/2024/05/hinh-anh-dep-ve-hoc-sinh-cap-3-1.jpg'
+            name={feedback.user.fullName}
+            time={feedback.createdAt}
+            rating={feedback.rating}
+            comment={feedback.comment}
+            nonFillColor='text-muted'
+          />
+        ))}
       </View>
     </ScrollView>
   );
