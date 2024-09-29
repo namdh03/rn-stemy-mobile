a17e2d79bbe8ed8492cf221bc5bf6a11430af00f
lachimo <lachimolalasii1410@gmail.com>
2024-09-29 07:33:02 +0700

    style: card product

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,28 +1,16 @@
 import { ScrollView, View } from 'react-native';
-import { execute } from 'graphql';
-
-import { useQuery } from '@tanstack/react-query';
 
 import images from '~assets/images';
 import Banner from '~components/customs/Banner';
 import Carousel from '~components/customs/Carousel';
 import Category from '~components/customs/Category';
-import ProductList from '~components/customs/ProductList';
 import SearchName from '~components/customs/SearchName';
 import { Bot, CircleX, Laptop, SlidersVertical, Wrench } from '~components/icons';
 import { Text } from '~components/ui/text';
-import { GET_PRODUCT_QUERY_KEY } from '~constants/product-query-key';
-import { GetProductQuery } from '~services/product.services';
 import { HomeScreenNavigationProps } from '~types/navigation.type';
 // import { removeAccessToken } from '~utils/token-storage';
 
-const HomeScreen = ({ navigation, route }: HomeScreenNavigationProps) => {
-  const { data, isFetching } = useQuery({
-    queryKey: [GET_PRODUCT_QUERY_KEY],
-    queryFn: () => execute(GetProductQuery, { id: Number(route.params.id) }),
-    select: (data) => data.data,
-  });
-
+const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
   const handlePress = () => {
     navigation.navigate('BottomTabStack', {
       screen: 'StoresScreen',
@@ -60,19 +48,9 @@ const HomeScreen = ({ navigation, route }: HomeScreenNavigationProps) => {
         </View>
       </View>
 
-      <ProductList title='Featured Product' data={data?.products.items || []} />
-
       <Banner imageUrl={images.bannerA} onPress={handlePress} />
 
-      <ProductList title='Best Sellers' data={data?.products.items || []} />
-
       <Banner imageUrl={images.bannerB} onPress={handlePress} />
-
-      <ProductList title='New Arrivals' data={data?.products.items || []} />
-
-      <ProductList title='Top Rated Product' data={data?.products.items || []} />
-
-      <ProductList title='Special Offers' data={data?.products.items || []} />
     </ScrollView>
   );
 };
