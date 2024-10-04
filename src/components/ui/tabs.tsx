e4e2367d0207ac59db59cb246973a553adaa33a6
namdh03 <duonghoangnam503@gmail.com>
2024-10-04 08:53:22 +0700

    chore: format code

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { TextClassContext } from '~components/ui/text';
+
 import * as TabsPrimitive from '@rn-primitives/tabs';
+
+import { TextClassContext } from '~components/ui/text';
 import { cn } from '~lib/utils';
 
 const Tabs = TabsPrimitive.Root;
@@ -13,7 +15,7 @@ const TabsList = React.forwardRef<
     ref={ref}
     className={cn(
       'web:inline-flex h-10 native:h-12 items-center justify-center rounded-md bg-muted p-1 native:px-1.5',
-      className
+      className,
     )}
     {...props}
   />
@@ -29,7 +31,7 @@ const TabsTrigger = React.forwardRef<
     <TextClassContext.Provider
       value={cn(
         'text-sm native:text-base font-medium text-muted-foreground web:transition-all',
-        value === props.value && 'text-foreground'
+        value === props.value && 'text-foreground',
       )}
     >
       <TabsPrimitive.Trigger
@@ -38,7 +40,7 @@ const TabsTrigger = React.forwardRef<
           'inline-flex items-center justify-center shadow-none web:whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium web:ring-offset-background web:transition-all web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
           props.disabled && 'web:pointer-events-none opacity-50',
           props.value === value && 'bg-background shadow-lg shadow-foreground/10',
-          className
+          className,
         )}
         {...props}
       />
@@ -55,7 +57,7 @@ const TabsContent = React.forwardRef<
     ref={ref}
     className={cn(
       'web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
-      className
+      className,
     )}
     {...props}
   />
