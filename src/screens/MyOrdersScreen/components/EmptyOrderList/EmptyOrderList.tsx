64e780be1e6bc1d21c726c731287d11c8e3166ad
namdh03 <duonghoangnam503@gmail.com>
2024-10-05 15:21:22 +0700

    [STEM-137] ui component order not found and order item skeleton

---
+++ src/screens/MyOrdersScreen/components/EmptyOrderList/EmptyOrderList.tsx
@@ -0,0 +1,22 @@
+import { View } from 'react-native';
+import { Image } from 'expo-image';
+
+import images from '~assets/images';
+import { Text } from '~components/ui/text';
+import constants from '~constants';
+
+const EmptyOrderList = () => (
+  <View className='flex-1 items-center py-10'>
+    <Image
+      source={images.searchOrders}
+      placeholder={{ blurhash: constants.EXPO_IMAGE.BLUR_HASH }}
+      style={{ width: 135, height: 128 }}
+      contentFit='cover'
+    />
+    <Text className='font-inter-regular text-center text-muted-foreground text-[14px] leading-[18px]'>
+      You don't have any orders in this category.
+    </Text>
+  </View>
+);
+
+export default EmptyOrderList;
