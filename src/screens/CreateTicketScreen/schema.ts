058fb3caa5950a67e1ca71fd64e35a7538a79d1e
namdh03 <duonghoangnam503@gmail.com>
2024-10-19 10:57:42 +0700

    [STEM-188] refactor anything related to ticket

---
+++ src/screens/CreateTicketScreen/schema.ts
@@ -0,0 +1,51 @@
+import * as FileSystem from 'expo-file-system';
+import { z } from 'zod';
+
+import constants from '~constants';
+
+const MAX_IMAGE_SIZE_MB = 1;
+const MAX_IMAGE_SIZE = MAX_IMAGE_SIZE_MB * 1024 * 1024;
+
+const ImageFileSchema = z.object({
+  uri: z.string(),
+  name: z.string(),
+  type: z.string(),
+});
+
+export const createTicketSchema = z.object({
+  categoryId: z.coerce
+    .number({
+      message: constants.MESSAGES.STAFF_TICKET.CATEGORY_ID,
+    })
+    .int({
+      message: constants.MESSAGES.STAFF_TICKET.CATEGORY_ID,
+    })
+    .positive({
+      message: constants.MESSAGES.STAFF_TICKET.CATEGORY_ID,
+    })
+    .min(1, {
+      message: constants.MESSAGES.STAFF_TICKET.CATEGORY_ID,
+    }),
+  title: z.string().min(1, {
+    message: constants.MESSAGES.STAFF_TICKET.TITLE_VALIDATE,
+  }),
+  description: z.string().min(1, {
+    message: constants.MESSAGES.STAFF_TICKET.DESCRIPTION_VALIDATE,
+  }),
+  images: z.array(ImageFileSchema).refine(
+    async (images) => {
+      for (const image of images) {
+        const info = await FileSystem.getInfoAsync(image.uri);
+        if (!info.exists || info.size > MAX_IMAGE_SIZE) {
+          return false;
+        }
+      }
+      return true;
+    },
+    {
+      message: `Each image must be smaller than ${MAX_IMAGE_SIZE_MB}MB.`,
+    },
+  ),
+});
+
+export type CreateTicketFormType = z.infer<typeof createTicketSchema>;
